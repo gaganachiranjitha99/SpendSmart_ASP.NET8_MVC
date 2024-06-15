b7ed56245897fefa02a22a4ffb8bac4e76b0dbb4
Gagana Chiranjitha <103405715+gaganachiranjitha99@users.noreply.github.com>
2024-06-15 22:55:36 +0530

    crud

---
+++ SpendSmart/SpendSmart/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using SpendSmart.Models;
+using System.Diagnostics;
+
+namespace SpendSmart.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly SpendSmartDbContext _context;
+        public HomeController(ILogger<HomeController> logger,SpendSmartDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+        public IActionResult Expenses() {
+            var allExpensses = _context.Expenses.ToList();
+            return View();
+        }
+        public IActionResult CreateEditExpense()
+        {
+            return View();
+        }
+        public IActionResult CreateEditExpenseForm(Expense model)
+        {
+            _context.Expenses.Add(model);
+            _context.SaveChanges();
+              return RedirectToAction("Expenses");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
